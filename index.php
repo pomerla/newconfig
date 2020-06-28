42595b6dcaf54207a1499ca5bb661f07b42c1796
Aleks Moroz <aleksmorro@gmail.com>
2020-06-28 18:59:03 +0300

    1
    1

--- index.php
+++ index.php
@@ -1,32 +1,97 @@
-<?
-  //$post['ip'] = $_SERVER["HTTP_CF_CONNECTING_IP"];
- $post['domain'] = $_SERVER['HTTP_HOST'];
- $post['referer'] = @$_SERVER['HTTP_REFERER'];
- $post['user_agent'] = $_SERVER['HTTP_USER_AGENT'];
- $post['headers'] = json_encode(apache_request_headers());
+<? require_once('config.php'); //  Подключаем конфигуратор ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "xhtml11.dtd">
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=windows-1251" />
+	<title><?= $product; // Продукт ?></title>
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.4.1/css/bootstrap.min.css" integrity="sha256-bZLfwXAP04zRMK2BjiO8iu9pf4FbLqX6zitd+tIvLhE=" crossorigin="anonymous" />
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+    
 
-
-
-if(isset($_SERVER["HTTP_CF_CONNECTING_IP"])) {	
- $post['ip'] = $_SERVER["HTTP_CF_CONNECTING_IP"];
-} else {
- $post['ip'] = $_SERVER["REMOTE_ADDR"];	
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+<style>
+.footer {
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  /* Set the fixed height of the footer here */
+  height: 75px;
+  background-color: #f5f5f5;
+  padding: 15px;
+  text-align: center;
 }
+</style>
+</head>
+<body>
+<div class="container">
+<nav class="navbar navbar-default">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#">
+        <img style="max-width: 30px;" alt="Brand" src="config/favicon.png">
+      </a>
+    </div>
+  </div>
+</nav>
+<div class="row">
+  <div class="col-md-6">
 
- $curl = curl_init('http://voronoa6.beget.tech/api/check_ip');
- curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
- curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
- curl_setopt($curl, CURLOPT_TIMEOUT, 60);
- curl_setopt($curl, CURLOPT_POST, true);
- curl_setopt($curl, CURLOPT_POSTFIELDS, $post);
- $json_reqest = curl_exec($curl);
- curl_close($curl);
- $api_reqest = json_decode($json_reqest);
 
- if(!@$api_reqest || @$api_reqest->white_link || @$api_reqest->result == 0){
- 	//require_once('config/white/index.php');
-    require_once('black.php');
- }else{
- 	//require_once('black.php');
-    require_once('config/white/index.php');
- }
+	<h3><?= $product; // Продукт ?></h1>
+    <p>Old prise:<?= $price_old; // Старая цена ?> <?= $currency; // Валюта ?></p>
+     <p>New prise:<?= $price_new; // Новая цена ?> <?= $currency; // Валюта ?></p>
+     <p>Sale:<?= $discount; // Скидка ?> %</p>
+     <p><?= htmlspecialchars_decode($value1_html) ?></p>
+     <p></p>
+     
+   </div> 
+    
+<div class="col-md-6">    
+<form class="m1-form" action="zakaz.php" method="post" onsubmit="if(this.name.value==''){alert('Введите Ваше имя!');return false}if(this.phone.value==''){alert('Введите Ваш номер телефона!');return false}return true;">
+	
+    <div class="form-group">
+        <label for="Name">Name</label>
+        <input class="form-control" type="text" name="name" placeholder="Name"/>
+    </div>
+    
+    <div class="form-group">
+        <label for="Phone">Phone</label>
+        <input class="form-control"  type="text" name="phone" placeholder="Phone"/>
+    </div>
+    
+    
+    <button class="btn btn-default" type="submit">submit</button>
+</form>
+  
+
+
+</div>
+</div>
+
+
+
+</div>
+<footer class="footer">
+    <div class="container">
+        <p><? lands::politics('red'); // Политика конфиденциальности ?></p>
+        <a class="develop"></a>
+    </div>
+</footer>
+
+<?  lands::footer(); // Финиш для скриптов /body ?>    
+<script  type="text/javascript">
+  var phone_masks = JSON.parse('{"RU":"+7(xxx)xxx-xx-xx","BY":"(+375 xx)xxx-xx-xx","KZ":"+7(xxx)xxx-xx-xx","UA":"+380","KG":"+996(xxx) xx-xx-xx","AZ":"+994(xx) xxx-xx-xx","AM":"(+374 xx) xxx-xxx","UZ":"(+998 xx) xxx-xx-xx","RO":"+40(xx)xxx-xx-xx","IT":"+39(xxx)xxx-xx-xx"}');    
+  var phone_numbers = JSON.parse('{"RU":"7","BY":"375","KZ":"7","UA":"380","KG":"996","AZ":"994","AM":"374","UZ":"998","RO":"40","IT":"39"}');        
+  var country_code_server ='UA';                 
+  $(document).ready(function () {        
+    $("input[name='phone']").val( phone_masks[country_code_server] );
+  });
+</script>    
+</body>
+</html>
